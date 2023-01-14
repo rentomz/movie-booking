63ec576b7e18f4df32cec1031739f531c7fee5ce
satrio <satrionugroho091@gmail.com>
2023-01-14 16:59:50 +0700

    https

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const apiKey = "31ccf49"
 
 
-axios.defaults.baseURL = `http://www.omdbapi.com/?apikey=${apiKey}&`;
+axios.defaults.baseURL = `https://www.omdbapi.com/?apikey=${apiKey}&`;
 
 
 
